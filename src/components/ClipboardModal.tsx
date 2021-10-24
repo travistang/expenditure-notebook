52587ae31626c94c8e8975dd12464b887bcd2e9f
Travis Tang <travis.tang@tum.de>
2021-10-24 16:48:17 +0200

    Add copy to clipboard function

---
+++ src/components/ClipboardModal.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import copy from "copy-to-clipboard";
+
+import { faClipboard } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import toast from "react-hot-toast";
+import { LocalStorageContext } from "../contexts/LocalStorageContext";
+import Modal, { ModalControlProps } from "./Modal";
+
+type Props = ModalControlProps;
+
+export default function ClipboardModal({ opened, onClose }: Props) {
+  const { store } = React.useContext(LocalStorageContext);
+  const copyToClipboard = () => {
+    copy(JSON.stringify(store));
+    toast.success("Copied store value to clipboard!");
+  };
+  return (
+    <Modal
+      title="Copy store content to clipboard"
+      opened={opened}
+      onClose={onClose}
+    >
+      <textarea
+        id="store-text"
+        className="shadow-inner rounded-lg bg-primary-500 code digit flex-1 resize-none"
+        value={JSON.stringify(store)}
+        contentEditable={false}
+      />
+      <div className="h-16 py-2 flex items-center justify-end">
+        <button
+          type="button"
+          onClick={copyToClipboard}
+          className="rounded-lg bg-secondary-500 gap-2 flex items-center p-2 h-full hover:bg-secondary-600"
+        >
+          <FontAwesomeIcon icon={faClipboard} />
+          Copy to clipboard
+        </button>
+      </div>
+    </Modal>
+  );
+}
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,8 @@
-import { faHome, faList } from "@fortawesome/free-solid-svg-icons";
+import { faClipboard, faHome, faList } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import { PageList } from "../pageList";
+import ClipboardModal from "./ClipboardModal";
 
 type Props = {
   currentPage: PageList;
@@ -14,16 +15,30 @@ export default function Header({ currentPage, goPage }: Props) {
       : PageList.HOME_PAGE;
   const goingPageIcon = goingPage === PageList.HOME_PAGE ? faHome : faList;
   const go = () => goPage(goingPage);
+
+  const [clipboardModalOpen, setClipboardModalOpen] = React.useState(false);
+
   return (
-    <header className="h-16 flex-0 center relative">
-      <span className="uppercase text-lg">Expenditure logbook</span>
-      <div className="absolute right-0 top-0 h-full horizontal-center gap-4 text-xl">
-        <FontAwesomeIcon
-          icon={goingPageIcon}
-          onClick={go}
-          className="cursor-pointer"
-        />
-      </div>
-    </header>
+    <>
+      <header className="h-16 flex-0 center relative">
+        <span className="uppercase text-lg">Expenditure logbook</span>
+        <div className="absolute right-0 top-0 h-full horizontal-center gap-4 text-xl">
+          <FontAwesomeIcon
+            icon={faClipboard}
+            className="cursor-pointer"
+            onClick={() => setClipboardModalOpen(true)}
+          />
+          <FontAwesomeIcon
+            icon={goingPageIcon}
+            onClick={go}
+            className="cursor-pointer"
+          />
+        </div>
+      </header>
+      <ClipboardModal
+        opened={clipboardModalOpen}
+        onClose={() => setClipboardModalOpen(false)}
+      />
+    </>
   );
 }

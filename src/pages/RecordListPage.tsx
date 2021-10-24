5845326ac2d5e27da94c66c492e953dee89a1229
Travis Tang <travis.tang@tum.de>
2021-10-25 00:54:52 +0200

    Cast date when filtering expenditures of months; Remove debugging list repetition

--- src/pages/RecordListPage.tsx
+++ src/pages/RecordListPage.tsx
@@ -12,11 +12,11 @@ export default function RecordListPage() {
   const oneMonthAgo = subMonths(now, 1);
   const expenditureOfTheMonth =
     store.expenditures
-      ?.filter((record) => isSameMonth(now, record.date))
+      ?.filter((record) => isSameMonth(now, new Date(record.date)))
       .reduce((total, record) => total + record.amount, 0) ?? 0;
   const expenditureOfLastMonth =
     store.expenditures
-      ?.filter((record) => isSameMonth(oneMonthAgo, record.date))
+      ?.filter((record) => isSameMonth(oneMonthAgo, new Date(record.date)))
       .reduce((total, record) => total + record.amount, 0) ?? 0;
   const expenditureDifferences = expenditureOfTheMonth - expenditureOfLastMonth;
   return (
@@ -38,22 +38,19 @@ export default function RecordListPage() {
           {expenditureDifferences.toFixed(2)}€
         </div>
       </div>
-      {Array(20)
-        .fill(expenditureList)
-        .flat()
-        ?.map((record) => (
-          <div className="flex-shrink-0 items-center grid grid-cols-12 h-16 border-t border-b border-primary-200">
-            <span className="col-span-3">
-              {format(new Date(record.date), "yyyy-MM-dd HH:mm")}
-            </span>
-            <span className="col-span-6 whitespace-nowrap overflow-hidden overflow-ellipsis">
-              {record.description}
-            </span>
-            <span className="col-span-3 whitespace-nowrap overflow-hidden overflow-ellipsis">
-              {record.amount}€
-            </span>
-          </div>
-        ))}
+      {expenditureList?.map((record) => (
+        <div className="flex-shrink-0 items-center grid grid-cols-12 h-16 border-t border-b border-primary-200">
+          <span className="col-span-3">
+            {format(new Date(record.date), "yyyy-MM-dd HH:mm")}
+          </span>
+          <span className="col-span-6 whitespace-nowrap overflow-hidden overflow-ellipsis">
+            {record.description}
+          </span>
+          <span className="col-span-3 whitespace-nowrap overflow-hidden overflow-ellipsis">
+            {record.amount}€
+          </span>
+        </div>
+      ))}
     </div>
   );
 }
